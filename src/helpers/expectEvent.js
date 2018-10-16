205adc31219c1512acceecb70c624c63f5163bea
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-16 10:08:33 -0400

    update expectEvent function with latest from openzeppelin-solidity

--- src/helpers/expectEvent.js
+++ src/helpers/expectEvent.js
@@ -1,5 +1,44 @@
-export default async function expectEvent (eventName, asyncFn) {
-  const { logs } = await asyncFn
-  const event = logs.find(e => e.event === eventName)
-  assert.isFalse(typeof event === 'undefined', 'Event must be triggered')
+import { BigNumber } from 'lk-web3-utils'
+import chai from 'chai'
+import chaiBigNumber from 'chai-bignumber'
+
+const should = chai.use(chaiBigNumber(BigNumber)).should()
+
+export function inLogs (logs, eventName, eventArgs = {}) {
+  const event = logs.find(function (e) {
+    if (e.event === eventName) {
+      for (const [k, v] of Object.entries(eventArgs)) {
+        contains(e.args, k, v)
+      }
+      return true
+    }
+  })
+  should.exist(event)
+  return event
 }
+
+export async function inTransaction (tx, eventName, eventArgs = {}) {
+  const { logs } = await tx
+  return inLogs(logs, eventName, eventArgs)
+}
+
+function contains (args, key, value) {
+  if (isBigNumber(args[key])) {
+    args[key].should.be.bignumber.equal(value)
+  } else {
+    args[key].should.be.equal(value)
+  }
+}
+
+function isBigNumber (object) {
+  return object.isBigNumber ||
+    object instanceof BigNumber ||
+    (object.constructor && object.constructor.name === 'BigNumber')
+}
+
+export default {
+  inLogs,
+  inTransaction,
+}
+
+
